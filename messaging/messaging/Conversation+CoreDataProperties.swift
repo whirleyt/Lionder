6c34b7feeddfc53d37b707b68d2f151c030edf66
Allen Wang <rw2848@columbia.edu>
2023-11-16 00:43:43 -0500

    Navigation Logic

---
+++ messaging/messaging/Conversation+CoreDataProperties.swift
@@ -0,0 +1,8 @@
+//
+//  Conversation+CoreDataProperties.swift
+//  messaging
+//
+//  Created by Runwei Wang on 11/15/23.
+//
+
+import Foundation
---
+++ messaging/messaging/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  messaging
+//
+//  Created by Runwei Wang on 11/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
