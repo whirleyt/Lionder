abc3b0393a455d2d84de736a3cbea273c538dfb7
Allen Wang <rw2848@columbia.edu>
2023-12-04 22:43:03 -0500

    resolved conflict. Changed userId to email. Fetch username from db based on userId: email

--- TeamAlgeria/TeamAlgeria/MessagingViewController.swift
+++ TeamAlgeria/TeamAlgeria/MessagingViewController.swift
@@ -19,52 +19,65 @@ class MessagingViewController: UIViewController, UITableViewDataSource, UITableV
     @IBAction func profileButtonTapped(_ sender: UIBarButtonItem) {
         performSegue(withIdentifier: "showProfile", sender: self)
     }
+    var emailForDB: String?
     var chatSessions: [ChatSession] = []
+    var userNames: [String: String] = [:]
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        var currentUserId = Auth.auth().currentUser?.uid
-        print(currentUserId ?? "noid")
-        //initializeDummyData()
-        fetchChatSessions()
+        //var currentUserId = Auth.auth().currentUser?.uid
+        //print(currentUserId ?? "noid")
+        
+        if let currentUserEmail = Auth.auth().currentUser?.email {
+            // Replace characters as needed to comply with Firebase key constraints
+            // For example, replacing '.' with '_'
+            emailForDB = currentUserEmail.replacingOccurrences(of: ".", with: "_dot_")
+
+            print("Current User Email: \(emailForDB ?? "NA")")
+            fetchChatSessions()
+        } else {
+            print("No user email found")
+        }
+        
+        initializeDummyData()
+        //fetchChatSessions()
     }
 
     
     private func initializeDummyData() {
-        guard let currentUserId = Auth.auth().currentUser?.uid else { return }
-        // let testUser1Id = "zacAGjInpgTMBzhz5vpNUY63E2k2" // user1
-        let testUser2Id = "qVF8ba0f4VhoWI8vN42zB7OL6QS2" // user2
-        let testUser3Id = "4gYJvxirqzSw1UwmgxvxI4qCcHh1" // user3
-        let testUser4Id = "ojwEdxSkLTfeHIFPppfyBibLDlx2" // user4
-    
-
+        //guard let currentUserId = Auth.auth().currentUser?.uid else { return }
+        // let testUser1Id = "user1@gmail_dot_com" // user1
+        let testUser2Id = "user2@gmail_dot_com" // user2
+        let testUser3Id = "user3@gmail_dot_com" // user3
+        let testUser4Id = "user4@gmail_dot_com" // user4
+        
         let session1: [String: Any] = [
             "sessionId": "session1",
-            "user1Id": currentUserId,
+            "user1Id": emailForDB ?? "NA",
             "user2Id": testUser2Id,
             "chatMessages": []
         ]
 
         let session2: [String: Any] = [
             "sessionId": "session2",
-            "user1Id": currentUserId,
+            "user1Id": emailForDB ?? "NA",
             "user2Id": testUser3Id,
             "chatMessages": []
         ]
         let session3: [String: Any] = [
             "sessionId": "session3",
-            "user1Id": currentUserId,
+            "user1Id": emailForDB ?? "NA",
             "user2Id": testUser4Id,
             "chatMessages": []
         ]
         
         chatSessions = [
-            ChatSession(sessionId: "session1", user1Id: currentUserId, user2Id: testUser2Id, chatMessages: []),
-            ChatSession(sessionId: "session2", user1Id: currentUserId, user2Id: testUser3Id, chatMessages: []),
-            ChatSession(sessionId: "session3", user1Id: currentUserId, user2Id: testUser4Id, chatMessages: [])
+            ChatSession(sessionId: "session1", user1Id: emailForDB, user2Id: testUser2Id, chatMessages: []),
+            ChatSession(sessionId: "session2", user1Id: emailForDB, user2Id: testUser3Id, chatMessages: []),
+            ChatSession(sessionId: "session3", user1Id: emailForDB, user2Id: testUser4Id, chatMessages: [])
         ]
 
         // Reference to the Firebase database location where chat sessions are stored
@@ -80,12 +93,27 @@ class MessagingViewController: UIViewController, UITableViewDataSource, UITableV
         }
     }
 
+    private func fetchUserName(email: String, completion: @escaping (String?) -> Void) {
+        let usersRef = Database.database().reference().child("user").child(email)
+        usersRef.observeSingleEvent(of: .value, with: { snapshot in
+            print("User data snapshot for email: \(email): \(snapshot)")
 
+            if let userData = snapshot.value as? [String: Any],
+               let userName = userData["name"] as? String {
+                completion(userName)
+            } else {
+                completion(nil)
+            }
+        })
+    }
 
-    
     private func fetchChatSessions() {
-        guard let currentUserId = Auth.auth().currentUser?.uid else { return }
-
+        
+        guard let emailForDB = emailForDB else {
+            print("User email is not available.")
+            return
+        }
+        
         let sessionsRef = Database.database().reference().child("chatSessions")
         sessionsRef.observe(.value, with: { [weak self] snapshot in
             var newChatSessions: [ChatSession] = []
@@ -96,7 +124,7 @@ class MessagingViewController: UIViewController, UITableViewDataSource, UITableV
                     print("Session Snapshot: \(snapshot)")
 
                     if let chatSession = ChatSession(snapshot: snapshot),
-                       chatSession.user1Id == currentUserId || chatSession.user2Id == currentUserId {
+                       chatSession.user1Id == emailForDB || chatSession.user2Id == emailForDB {
                         newChatSessions.append(chatSession)
                     }
                 }
@@ -126,6 +154,19 @@ class MessagingViewController: UIViewController, UITableViewDataSource, UITableV
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChatTableViewCell", for: indexPath) as! ChatTableViewCell
         let chatSession = chatSessions[indexPath.row]
+        
+        let otherUserEmail = (chatSession.user1Id == emailForDB ? chatSession.user2Id : chatSession.user1Id)!
+
+        
+        fetchUserName(email: otherUserEmail) { userName in
+            DispatchQueue.main.async {
+                if let name = userName {
+                    cell.nameLabel.text = name
+                } else {
+                    cell.nameLabel.text = "Unknown User"
+                }
+            }
+        }
 
         if let lastMessage = chatSession.chatMessages.last {
             cell.lastMessageLabel.text = lastMessage.content
