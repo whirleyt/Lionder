6a3272a8eeed78dd5c87c8aff6396d78e6a5a6aa
Nicole Neil <njn2122@columbia.edu>
2023-12-05 09:53:39 -0500

    Calendar further edits

---
+++ TeamAlgeria/TeamAlgeria/CalendarEventsService.swift
@@ -0,0 +1,8 @@
+//
+//  CalendarEventsService.swift
+//  TeamAlgeria
+//
+//  Created by Nicole Neil on 12/5/23.
+//
+
+import Foundation
---
+++ TeamAlgeria/TeamAlgeria/EventsModel.swift
@@ -0,0 +1,8 @@
+//
+//  EventsModel.swift
+//  TeamAlgeria
+//
+//  Created by Nicole Neil on 12/5/23.
+//
+
+import Foundation
